49d6d14d548ba79e4260ccb35383f9d57a9864a6
Achraf <achraf@example.com>
2026-05-06 22:17:19 +0100

    Enhance JustificationResource to include absent date, subject, and student names for improved data retrieval

--- app/Http/Resources/JustificationResource.php
+++ app/Http/Resources/JustificationResource.php
@@ -18,9 +18,15 @@ class JustificationResource extends JsonResource
             'attendance_id' => $this->attendance_id,
 
             'reviewed_at' => $this->reviewed_at,
+            'absent_date' => $this->attendance?->date,
+
+            'subject' => $this->attendance?->subject,
 
             'parent_id' => $this->parent_id,
             'reviewed_by' => $this->reviewed_by,
+
+            'student_first_name' => $this->attendance?->student?->user?->first_name,
+            'student_last_name'  => $this->attendance?->student?->user?->last_name,
         ];
     }
 }
